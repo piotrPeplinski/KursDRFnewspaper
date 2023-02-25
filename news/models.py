a70eb0c607e43c7f904a85782cb0b43f72c01273
Piotr Pepliński <p.peplaj@gmail.com>
2023-02-25 16:14:17 +0100

    created project with one app, created a model

---
+++ news/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='articles')
+    # adding related_name is neccessary for reverse relationship to work in serializer (PrimaryKeyRelatedField)
+    
