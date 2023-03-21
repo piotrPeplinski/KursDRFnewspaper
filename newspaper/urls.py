a18db52b971de869a718307be7b4652905739c67
Piotr Pepliński <p.peplaj@gmail.com>
2023-03-21 10:18:34 +0100

    token authentication - added ability to obtain token by existing user

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
+    path('get-token/', views.obtain_auth_token),#post with username and pswrd
 ]
