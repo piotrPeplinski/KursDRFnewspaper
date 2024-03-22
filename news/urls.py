e7a659d938df9814b3978c5b3c4816eec4fdb4e7
Piotr Pepliński <p.peplaj@gmail.com>
2024-03-22 14:31:45 +0100

    added viewset and router

--- news/urls.py
+++ news/urls.py
@@ -1,13 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+
 
 urlpatterns = [
     path('articles/', views.ArticleList.as_view(), name='articles'),
     path('articles/<int:pk>/', views.ArticleDetail.as_view(), name='articleDetail'),
-    path('users/', views.ListCreateUser.as_view(), name='users'),
+    # path('users/', views.ListCreateUser.as_view(), name='users'),
     path('update-user/<int:pk>/', views.UpdateUser.as_view(), name='update-user'),
     path('', views.APIRoot.as_view(), name='apiRoot'),
+    path('auth/', include(router.urls)),
+
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- news/views.py
+++ news/views.py
@@ -7,13 +7,13 @@ from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from .permissions import IsOwnerOrReadOnly
 from .paginators import ArticlePaginator
-from rest_framework import filters
+from rest_framework import filters, viewsets
 
 
 class APIRoot(APIView):
     def get(self, request, format=None):
         links = {
-            'users': reverse('users', request=request, format=format),
+            'users': reverse('user-list', request=request, format=format),
             'articles': reverse('articles', request=request, format=format),
             # add more links to other endpoints as needed
         }
@@ -23,7 +23,7 @@ class APIRoot(APIView):
 class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    #pagination_class = ArticlePaginator
+    # pagination_class = ArticlePaginator
     # filter_backends = [filters.OrderingFilter]
     # ordering = ['-date']
 
@@ -45,6 +45,16 @@ class ListCreateUser(generics.ListCreateAPIView):
         reqMethod = self.request.method
         return [permissions.IsAdminUser()] if reqMethod == 'GET' else [permissions.AllowAny()]
 
+
 class UpdateUser(generics.UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
